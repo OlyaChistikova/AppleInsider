6e0ebd11a98b28ea4560760528b8b0d0606908ef
Olya Chistikova <olya.chistikova@mail.ru>
2025-08-29 23:25:51 +0300

    Add AppleInsider

---
+++ src/test/java/appleInsider/AppleTest.java
@@ -0,0 +1,18 @@
+package appleInsider;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppleTest extends BaseTest{
+    private final static String BASE_URL = "https://appleinsider.ru/";
+    private final static String SEARCH_STRING = "Чем iPhone 13 отличается от iPhone 12";
+    private final static String EXPECTED_WORD = "iphone-13";
+
+    @Test
+    public void checkHref(){
+        Assert.assertTrue(new MainPage(BASE_URL)
+                .search(SEARCH_STRING)
+                .getHrefFirstArticle()
+                .contains(EXPECTED_WORD));
+    }
+}
---
+++ src/test/java/appleInsider/SearchPage.java
@@ -0,0 +1,16 @@
+package appleInsider;
+
+import com.codeborne.selenide.ElementsCollection;
+
+import static com.codeborne.selenide.Selenide.$$x;
+
+public class SearchPage {
+    private final ElementsCollection articleTitles = $$x("//h2//a");
+
+    /**
+     * Возвращает href из первой статьи
+     */
+    public String getHrefFirstArticle(){
+        return articleTitles.first().getAttribute("href");
+    }
+}
