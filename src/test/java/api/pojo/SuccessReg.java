44dab0a03eaffbbab8558cb7080884db9512073d
Olya Chistikova <olya.chistikova@mail.ru>
2025-09-16 14:40:17 +0300

    Add pojo

---
+++ src/test/java/api/pojo/SuccessReg.java
@@ -0,0 +1,22 @@
+package api.pojo;
+
+public class SuccessReg {
+    private Integer id;
+    private String token;
+
+    public SuccessReg(Integer id, String token) {
+        this.id = id;
+        this.token = token;
+    }
+
+    public SuccessReg() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
---
+++ src/test/java/api/pojo/UserTimeResponse.java
@@ -0,0 +1,17 @@
+package api.pojo;
+
+public class UserTimeResponse extends UserTime {
+    private String updatedAt;
+
+    public UserTimeResponse(String name, String job, String updatedAt) {
+        super(name, job);
+        this.updatedAt = updatedAt;
+    }
+    public UserTimeResponse(){
+        super();
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+}
